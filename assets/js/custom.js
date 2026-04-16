2fca4cd18ec0bd0ed3ba6af0d3d92fdfab749614
Md-Sakibul-Islam <sakibul.diu@gmail.com>
2026-04-16 21:28:44 +0600

    video popup add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,42 +1,63 @@
 (function ($) {
-    "use strict"
+  "use strict"
 
 
-    // data bg img
-    $("[data-bg-img]").each(function () {
-        $(this).css({
-            "background-image": "url(" + $(this).attr("data-bg-img") + ")",
-            "background-repeat": "no-repeat"
-        });
+  // data bg img
+  $("[data-bg-img]").each(function () {
+    $(this).css({
+      "background-image": "url(" + $(this).attr("data-bg-img") + ")",
+      "background-repeat": "no-repeat"
     });
-
- 
-    //data bg color
-    $("[data-bg-color]").each(function () {
-        $(this).css("background-color", $(this).attr("data-bg-color"));
-    });
+  });
 
 
-    // services slider 
-      var swiper = new Swiper(".ts-services-active", {
-      slidesPerView: 3,
-      spaceBetween: 24,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+  //data bg color
+  $("[data-bg-color]").each(function () {
+    $(this).css("background-color", $(this).attr("data-bg-color"));
+  });
+
+  // Image popup
+  $('.ts-image-popup').magnificPopup({
+    type: 'image'
+    // other options
+  });
+
+  // video popup
+  $('.ts-video-popup').magnificPopup({
+    type: 'iframe',
+    iframe: {
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+        },
       },
-    });
-    
+      srcAction: 'iframe_src',
+    }
+  });
 
-    // Client slider 
-      var swiper = new Swiper(".ts-client-active", {
-      slidesPerView: 6,
-      spaceBetween: 80,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+
+  // services slider 
+  var swiper = new Swiper(".ts-services-active", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  // Client slider 
+  var swiper = new Swiper(".ts-client-active", {
+    slidesPerView: 6,
+    spaceBetween: 80,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 
 
